e12a2e1c2b92a3a70646c7b55b09b2e293305e1c
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-22 22:56:26 -0700

    build(cargo): update packages

--- packages/istanbul-oxi-instrument-wasm/src/lib.rs
+++ packages/istanbul-oxi-instrument-wasm/src/lib.rs
@@ -1,6 +1,7 @@
+use istanbul_oxi_instrument::program_visitor;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(js_name="programVisitorSpec")]
+#[wasm_bindgen(js_name = "programVisitorSpec")]
 pub fn program_visitor_spec() {
-
+    program_visitor();
 }
