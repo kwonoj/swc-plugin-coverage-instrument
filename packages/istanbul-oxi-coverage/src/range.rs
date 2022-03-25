6b513f913d6562878bf7c2e8f547388308fe0740
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-25 05:24:11 +0000

    fix(coverage): use idxmap for ordered coverage map merge

--- packages/istanbul-oxi-coverage/src/range.rs
+++ packages/istanbul-oxi-coverage/src/range.rs
@@ -1,16 +1,27 @@
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default, Debug)]
 pub struct Location {
     pub(crate) line: u32,
     pub(crate) column: u32,
 }
+impl Location {
+    pub fn default() -> Location {
+        Location { line: 0, column: 0 }
+    }
+}
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default, Debug)]
 pub struct Range {
     pub(crate) start: Location,
     pub(crate) end: Location,
 }
 
 impl Range {
+    pub fn default() -> Range {
+        Range {
+            start: Default::default(),
+            end: Default::default(),
+        }
+    }
     pub fn new(start_line: u32, start_column: u32, end_line: u32, end_column: u32) -> Range {
         Range {
             start: Location {
