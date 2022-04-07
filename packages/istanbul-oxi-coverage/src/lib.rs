26983da71c10549490b3dee5fab81438f2ca772f
OJ Kwon <kwon.ohjoong@gmail.com>
2022-04-06 17:50:12 -0700

    refactor(coverage): expose struct public

--- packages/istanbul-oxi-coverage/src/lib.rs
+++ packages/istanbul-oxi-coverage/src/lib.rs
@@ -6,10 +6,9 @@ mod percent;
 mod range;
 mod types;
 
-use coverage::*;
 pub use coverage_map::CoverageMap;
 use coverage_summary::*;
 pub use file_coverage::FileCoverage;
 use percent::*;
 pub use range::*;
-use types::*;
+pub use types::*;
