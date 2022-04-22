cc986dbf5d68f8cb5951050ea7cfe8c60aa6e844
OJ Kwon <kwon.ohjoong@gmail.com>
2022-04-22 00:24:49 -0700

    docs(template): add comments

--- packages/swc-plugin-istanbul-oxi/src/template/mod.rs
+++ packages/swc-plugin-istanbul-oxi/src/template/mod.rs
@@ -1,5 +1,7 @@
+//! Utility fn to create an AST for instrumentation wrapper object injection.
+
 mod create_assignment_stmt;
 mod create_coverage_data_object;
-pub(crate) mod create_global_stmt_template;
 
 pub(crate) mod create_coverage_fn_decl;
+pub(crate) mod create_global_stmt_template;
