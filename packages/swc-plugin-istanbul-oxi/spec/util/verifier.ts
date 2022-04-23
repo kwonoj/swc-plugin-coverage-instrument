c46f3aaa8e7424f7439dbc27f4c5e0ac2c791095
OJ Kwon <kwon.ohjoong@gmail.com>
2022-04-23 14:24:56 -0700

    test(verifier): use debug build

--- packages/swc-plugin-istanbul-oxi/spec/util/verifier.ts
+++ packages/swc-plugin-istanbul-oxi/spec/util/verifier.ts
@@ -9,7 +9,7 @@ const clone: typeof import("lodash.clone") = require("lodash.clone");
 
 const pluginBinary = path.resolve(
   __dirname,
-  "../../../../target/wasm32-wasi/release/swc_plugin_istanbul_oxi.wasm"
+  "../../../../target/wasm32-wasi/debug/swc_plugin_istanbul_oxi.wasm"
 );
 
 /// Mimic instrumenter.
@@ -249,4 +249,4 @@ const create = (code, options = {}, instrumentOptions = {}, inputSourceMap) => {
   });
 };
 
-export { create };
+export { create, instrumentSync };
