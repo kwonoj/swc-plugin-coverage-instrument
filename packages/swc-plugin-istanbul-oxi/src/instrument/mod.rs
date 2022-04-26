add40c65bf37cec7a98e10f7fe691d4053bb20ef
OJ Kwon <kwon.ohjoong@gmail.com>
2022-04-25 17:24:14 -0700

    feat(instrument): support branch counter expr

--- packages/swc-plugin-istanbul-oxi/src/instrument/mod.rs
+++ packages/swc-plugin-istanbul-oxi/src/instrument/mod.rs
@@ -1,48 +1,62 @@
 use swc_plugin::{ast::*, syntax_pos::DUMMY_SP};
 
-use crate::coverage_visitor::UnknownReserved;
-
 /// Creates a expr like `cov_17709493053001988098().s[0]++;`
+/// idx indicates index of vec-based counters (i.e branches).
+/// If it exists, creates a expr with idx like
+/// 1cov_17709493053001988098().b[0][idx]++;` instead.
 pub(crate) fn build_increase_expression_expr(
     type_ident: &Ident,
     id: u32,
     var_name: &Ident,
-    i: Option<UnknownReserved>,
+    idx: Option<u32>,
 ) -> Expr {
-    if let Some(_i) = i {
-        todo!("Not implemented yet!")
-    } else {
-        let call = CallExpr {
-            span: DUMMY_SP,
-            callee: Callee::Expr(Box::new(Expr::Ident(var_name.clone()))),
-            args: vec![],
-            type_args: None,
-        };
+    let call = CallExpr {
+        span: DUMMY_SP,
+        callee: Callee::Expr(Box::new(Expr::Ident(var_name.clone()))),
+        args: vec![],
+        type_args: None,
+    };
 
-        let c = MemberExpr {
-            span: DUMMY_SP,
-            obj: Box::new(Expr::Call(call)),
-            prop: MemberProp::Ident(type_ident.clone()),
-        };
+    let c = MemberExpr {
+        span: DUMMY_SP,
+        obj: Box::new(Expr::Call(call)),
+        prop: MemberProp::Ident(type_ident.clone()),
+    };
 
-        let expr = MemberExpr {
+    let expr = MemberExpr {
+        span: DUMMY_SP,
+        obj: Box::new(Expr::Member(c)),
+        prop: MemberProp::Computed(ComputedPropName {
             span: DUMMY_SP,
-            obj: Box::new(Expr::Member(c)),
+            expr: Box::new(Expr::Lit(Lit::Num(Number {
+                span: DUMMY_SP,
+                value: id as f64,
+                raw: None,
+            }))),
+        }),
+    };
+
+    let expr = if let Some(idx) = idx {
+        MemberExpr {
+            span: DUMMY_SP,
+            obj: Box::new(Expr::Member(expr)),
             prop: MemberProp::Computed(ComputedPropName {
                 span: DUMMY_SP,
                 expr: Box::new(Expr::Lit(Lit::Num(Number {
                     span: DUMMY_SP,
-                    value: id as f64,
+                    value: idx as f64,
                     raw: None,
                 }))),
             }),
-        };
+        }
+    } else {
+        expr
+    };
 
-        Expr::Update(UpdateExpr {
-            span: DUMMY_SP,
-            op: UpdateOp::PlusPlus,
-            prefix: false,
-            arg: Box::new(Expr::Member(expr)),
-        })
-    }
+    Expr::Update(UpdateExpr {
+        span: DUMMY_SP,
+        op: UpdateOp::PlusPlus,
+        prefix: false,
+        arg: Box::new(Expr::Member(expr)),
+    })
 }
