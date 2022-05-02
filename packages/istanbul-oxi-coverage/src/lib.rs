f62e994605a3c79fb472fa4e5fc68354f60bb7ea
OJ Kwon <kwon.ohjoong@gmail.com>
2022-05-02 14:36:12 -0700

    feat(coverage): support sourcemap

--- packages/istanbul-oxi-coverage/src/lib.rs
+++ packages/istanbul-oxi-coverage/src/lib.rs
@@ -4,6 +4,7 @@ mod coverage_summary;
 mod file_coverage;
 mod percent;
 mod range;
+mod source_map;
 pub mod types;
 
 pub use coverage_map::CoverageMap;
@@ -11,4 +12,5 @@ use coverage_summary::*;
 pub use file_coverage::FileCoverage;
 use percent::*;
 pub use range::*;
+pub use source_map::SourceMap;
 pub use types::*;
