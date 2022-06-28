8ac43774d24efb582f118a4bbc6d1bcadf63815a
OJ Kwon <kwon.ohjoong@gmail.com>
2022-06-28 16:49:44 -0700

    fix(template): use fn ctor for global assignment

--- packages/swc-coverage-instrument/src/coverage_template/create_global_stmt_template.rs
+++ packages/swc-coverage-instrument/src/coverage_template/create_global_stmt_template.rs
@@ -1,5 +1,5 @@
 use swc_common::{util::take::Take, DUMMY_SP};
-use swc_ecmascript::ast::*;
+use swc_ecmascript::{ast::*, utils::quote_ident};
 
 use crate::constants::idents::IDENT_GLOBAL;
 
@@ -8,8 +8,13 @@ use super::create_assignment_stmt::create_assignment_stmt;
 /// Creates an assignment statement for the global scope lookup function
 /// `var global = new Function("return $global_coverage_scope")();`
 pub fn create_global_stmt_template(coverage_global_scope: &str) -> Stmt {
+    // Note: we don't support function template based on scoped binding
+    // like https://github.com/istanbuljs/istanbuljs/blob/c7693d4608979ab73ebb310e0a1647e2c51f31b6/packages/istanbul-lib-instrument/src/visitor.js#L793=
+    // due to scope checking is tricky.
+    let fn_ctor = quote_ident!("((function(){}).constructor)");
+
     let expr = Expr::New(NewExpr {
-        callee: Box::new(Expr::Ident(Ident::new("Function".into(), DUMMY_SP))),
+        callee: Box::new(Expr::Ident(fn_ctor)),
         args: Some(vec![ExprOrSpread {
             spread: None,
             expr: Box::new(Expr::Lit(Lit::Str(Str {
